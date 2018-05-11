f2151c48f972563042db3cc40a05ff6b416b66e4
Alex Konkin <alexkonkin@gmail.com>
2018-05-11 17:03:02 +0300

    css and js folders and files were added

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by oleksiy.konkin on 5/11/2018.
+ */
+/*
+ https://freshman.tech/random-quote-machine/
+ https://market.mashape.com/andruxnet/random-famous-quotes#get-endpoint
+ https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous
+
+ */
+
+
+console.log("test");
